1e55389ad25d28558f6d603d25856151237ff25c
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-16 12:56:16 +0100

    Created a code that prints string using recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,6 +4,7 @@
 
 /**
  * -puts_recursion - prints a string recursively to the stdout.
+ * @*s: is the pointer to the string giving by the test funtcion
  * Description: this function uses _putchar fun to print an array or chars
  * to stdout using recursion.
  * Return: it doesn't return anything
@@ -11,11 +12,13 @@
 
  void _puts_recursion(char *s)
  {
-    int i; 
-    for(i = 0; i < sizeof(s) / sizeof(*s); i++)
+    if (*s == '\0')
     {
-        _putchar(s[i]);
+        _putchar('\n');
     }
-    _putchar('\n');
-   
+    else
+    {
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    }
  }

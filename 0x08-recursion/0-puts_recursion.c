06d67ff938ed911e090773b096162d6a42912e8f
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-16 13:16:58 +0100

    Fixed a bug

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,22 +3,21 @@
 
 
 /**
- * -puts_recursion - prints a string recursively to the stdout.
- * @*s: is the pointer to the string giving by the test funtcion
+ * _puts_recursion - prints a string recursively to the stdout.
+ * @s: is the pointer to the string giving by the test funtcion
  * Description: this function uses _putchar fun to print an array or chars
  * to stdout using recursion.
  * Return: it doesn't return anything
  */
 
- void _puts_recursion(char *s)
- {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
- }
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	_putchar('');
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	_putchar('\n');
+}

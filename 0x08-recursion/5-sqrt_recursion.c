011710be57ef9e659f2250eaaf2e1febd89d42b2
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-16 17:48:22 +0100

    Get the square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+int square(int num, int num1);
+
+/**
+ * _sqrt_recursion- calculates the square root of a given number
+ * @n: the number which will be squared rooted
+ * Return: return the square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square - return the square root of n
+ * @num: the num which we should find its root
+ * @num1: initially is one but will change accordinly
+ * Return: the square root of num
+ */
+
+int square(int num, int num1)
+{
+	if (num1 * num1 == num)
+	{
+		return (num1);
+	}
+	else if (num1 * num1 < num)
+	{
+		square(num, (num1 + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}

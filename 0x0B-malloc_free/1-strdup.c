461aaf82583a2e97ae1fd20a3513893b5027404c
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-19 15:12:04 +0100

    Duplicate an array of chars using malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * _strdup - duplicates a str passed to it using malloc
+ * @str: the string to be duplicated
+ * Return: NULL if it falis, a pointer to the new array if success
+ */
+
+char *_strdup(char *str)
+{
+	/* this is the pointer that will point to the new array */
+	char *ptr;
+	unsigned int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(str));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < sizeof(str); i++)
+	{
+		ptr[i] = str[i];
+	}
+
+	return (ptr);
+
+}

39d7af731031fbf45e5f556460b7505f5c5e55e5
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-19 21:12:29 +0100

    Free allocated memo by alloc-grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees the memory allocated by another function.
+ * @grid: function to be freed
+ * @height: number of row of 2D array
+ * Return: nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+}

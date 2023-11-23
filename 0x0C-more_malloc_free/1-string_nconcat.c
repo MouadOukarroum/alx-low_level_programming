0cf9d1e7bb75bb56e1a074925525f4fff5422c58
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-23 20:37:52 +0100

    Fix a bug

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 	/* assign s1 and n of bytes from s2 to ptr*/
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
 
 	for (j = 0; j < n; j++)

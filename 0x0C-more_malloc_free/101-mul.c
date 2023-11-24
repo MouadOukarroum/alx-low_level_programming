99fc4013a45bb1d63fe1f8d1bb0fc6cff85b5277
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-24 21:07:40 +0100

    Multiplication is fun

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints a number
+ * @n: the number to print.
+ * Return: nothing; it's a void funtion
+ */
+
+void print_number(int n);
+
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Error, 98 on failure, mul of num1 and num2 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	char er[] = "Error";
+	unsigned int i, j, k;
+	int num1, num2, res;
+
+	if (argc != 3)
+	{
+		for (i = 0; i < 5; i++)
+			_putchar(er[i]);
+		_putchar('\n');
+		exit(98);
+	}
+
+	for (j = 0; argv[1][j]; j++)
+	{
+		if(!isdigit(argv[1][j]))
+		{
+			for (i = 0; i < 5; i++)
+				_putchar(er[i]);
+			_putchar('\n');
+			exit(98);	
+		}	
+	}
+
+	for (k = 0; argv[2][k]; k++)
+	{
+		if(!isdigit(argv[2][k]))
+		{
+			for (i = 0; i < 5; i++)
+				_putchar(er[i]);
+			_putchar('\n');
+			exit(98);
+		}
+	}
+
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	res = num1 * num2;
+
+	print_number(res);
+	_putchar('\n');
+	return (0);
+}
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}

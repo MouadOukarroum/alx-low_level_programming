a14c25475842a88a2f6d79972b7fa132b93b5f22
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-24 19:08:39 +0100

    Fixed a bug

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		newPtr = ptr;
 		for (i = 0; i < old_size; i++)
-			ptor[i] =newPtr[i];
+			ptor[i] = newPtr[i];
 	}
 	free(ptr);
 	ptoreturn = ptor;

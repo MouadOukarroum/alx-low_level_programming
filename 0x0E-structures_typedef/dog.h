57e8bed3b138a4ebcad867886c255e3b97a37f6b
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-28 22:37:03 +0100

    First Task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: dog's name
+ * @age: age of the dog
+ * @owner: owner's name
+ *
+ *Description: just a dog struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

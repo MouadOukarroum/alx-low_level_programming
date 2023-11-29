e3a89417d94a6bb1572494c9e790dc5fe44c1fa5
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-29 23:36:52 +0100

    Second Task is Done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,21 +12,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
-	int index;
+	int i = 0;
 
 	if (array && size && cmp)
-	{
-		if (size <= 0)
-			return (-1);
-		for (i = 0; i < size; i++)
+		while (i < size)
 		{
-			if (cmp(array[i]) != 0)
-			{
-				index = i;
-				break;
-			}
+			if(cmp(array[i]))
+				return (i);
+			i++;
 		}
-	}
-	return (index);
+	return (-1);
 }

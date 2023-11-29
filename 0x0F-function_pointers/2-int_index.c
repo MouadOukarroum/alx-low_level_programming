3b7088351fa471846687f6ad03904fda54c734c2
MouadOukarroum <oukarroummouad@gmail.com>
2023-11-29 23:30:16 +0100

    Third Task is Done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,6 +13,7 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+	int index;
 
 	if (array && size && cmp)
 	{
@@ -20,10 +21,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (-1);
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]))
-				return (i);
+			if (cmp(array[i]) != 0)
+			{
+				index = i;
+				break;
+			}
 		}
-		return (-1);
 	}
-
+	return (index);	
 }

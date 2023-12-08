c629bb7caf7fca768dfd6e8f74fa621e3cfce485
MouadOukarroum <oukarroummouad@gmail.com>
2023-12-08 17:52:21 +0100

    There is always a way to do anything

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void myConst(void) __attribute__((constructor));
 
 void myConst(void)
 {
-	printf("You're beat! and yet, you must allow,\n")
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }

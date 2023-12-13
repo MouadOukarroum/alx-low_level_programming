c3d06d0e0da85b2b3df825c6d0d949a0ddf2a534
MouadOukarroum <oukarroummouad@gmail.com>
2023-12-13 23:20:19 +0100

    Print linked list of integers

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+/**
+ * print_listint - prints a list of numbers
+ * @h: the head of the list
+ * Return: the number of nodes
+ */
+
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int i = 0;
+
+	while(h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

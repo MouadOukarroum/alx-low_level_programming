a81e2e1a5bef2ffe4a995d6775b8b5ce4d81a4b2
MouadOukarroum <oukarroummouad@gmail.com>
2023-12-14 01:55:41 +0100

    Fixed a bug in TASK 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, const int index)
 {
 	listint_t *node;
-	unsigned int n;
+	int n;
 
 	for (node = head, n = 0; node && n < index; node = node->next, n++)
 		;
